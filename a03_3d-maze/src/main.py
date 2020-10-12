fd511c800aa7b3fa28f608ea7d1aa259af5398fd
john <re_pel@outlook.com>
2020-10-12 16:35:02 +0000

    fixed the suggestions from PR and added optional arguments for entering level

--- a03_3d-maze/src/main.py
+++ a03_3d-maze/src/main.py
@@ -1,3 +1,6 @@
+import getopt
+import sys
+
 import glm
 import pygame
 from world import World
@@ -46,32 +49,62 @@ def main():
     world.cleanup()
 
 
-def choose_level():
-    print('Please choose a level!')
-    print('Choose:\n\t1 for Beginner (Noob)\n\t2 for Intermediate\n\t3 for Pro\n\n')
+def get_level_from_cmd(argv):
+    level = 0
+    try:
+        opts, args = getopt.getopt(argv, "l:", ["level="])
+    except getopt.GetoptError:
+        print('main.py --level <level>\nor \nmain.py -l <level>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt not in ("-l", "--level"):
+            print('main.py --level <level>\nor \nmain.py -l <level>')
+            sys.exit()
+        elif opt in ("-l", "--level"):
+            try:
+                level = int(arg)
+            except ValueError:
+                print('Level argument must be an integer!')
+                sys.exit()
+    return level
+
+
+def choose_level(argv):
     global LEVEL
-    while True:
-        show = False
-        val = 0
-        try:
-            val = int(input('Enter your choice: '))
-        except ValueError:
-            print('You should choose a number between 1, 2 or 3 inclusive!')
+    got_arg = False
+    level = get_level_from_cmd(argv)
+    if 3 >= level > 0:
+        LEVEL = level
+        got_arg = True
+    elif not got_arg and (level < 0 or level > 3):
+        print('The --level argument should be either 1, 2 or 3')
+        sys.exit()
+    if not got_arg:
+        print('Please choose a level!')
+        print('Choose:\n\t1 for Beginner (Noob)\n\t2 for Intermediate\n\t3 for Pro\n\n')
+        while True:
             show = False
-        if 3 >= val > 0:
-            LEVEL = val
-            if val == 3:
-                print('Excellent choice!')
+            val = 0
+            try:
+                val = int(input('Enter your choice: '))
+            except ValueError:
+                print('You should choose a number between 1, 2 or 3 inclusive!')
+                show = False
+            if 3 >= val > 0:
+                LEVEL = val
+                if val == 3:
+                    print('Excellent choice!')
+                    break
                 break
-            break
-        if val < 0 or val > 3:
-            show = True
-        if show:
-            print('You should choose either 1, 2 or 3!')
+            if val < 0 or val > 3:
+                show = True
+            if show:
+                print('You should choose either 1, 2 or 3!')
 
 
 if __name__ == '__main__':
-    choose_level()
-    print('Press Escape to quit!')
+    choose_level(sys.argv[1:])
+    print('\nPress Escape to quit!\n')
     main()
     pygame.quit()
+    print('You quit the game!')
