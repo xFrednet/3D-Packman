5b32a032b46f17c40186009be4e85752b48eebb1
xFrednet <xFrednet@gmail.com>
2020-10-12 23:22:38 +0000

    Fixed for windows

--- a03_3d-maze/src/sound_manager.py
+++ a03_3d-maze/src/sound_manager.py
@@ -1,17 +1,17 @@
 import pygame.mixer
-
+import sys
 
 class SoundManager:
     def __init__(self):
         pygame.mixer.init()
-        self.path = 'res/sound/'
+        self.path = sys.path[0] + '/res/sound/'
         self.game_over = pygame.mixer.Sound(self.path + 'game_over.wav')
         self.damage = pygame.mixer.Sound(self.path + 'damage.wav')
         self.start = pygame.mixer.Sound(self.path + 'start.wav')
         self.win = pygame.mixer.Sound(self.path + 'win.wav')
 
     def play_music(self):
-        pygame.mixer.music.load(self.path + 'game.wav')
+        pygame.mixer.music.load(self.path + '/game.wav')
         pygame.mixer.music.play(-1)
 
     def play_sound(self, sound):
